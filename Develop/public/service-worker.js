5c3a736d43daf8c50cccace94626e4d4bd1a7271
echo <miyunniecho@gmail.com>
2021-04-22 22:54:18 -0700

    update files to cache

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -1,16 +1,20 @@
-const CACHE_NAME = 'my-site-cache-v2';
-const DATA_CACHE_NAME = 'data-cache-v2';
+const CACHE_NAME = 'my-site-cache-v1';
+const DATA_CACHE_NAME = 'data-cache-v1';
 const FILES_TO_CACHE = [
   '/',
   '/index.html',
-  '/favicon.ico',
   '/manifest.json',
-  '/app.js',
-  '/assets/css/style.css',
-  '/assets/images/icons/icon-72x72.png',
-  '/assets/images/icons/icon-96x96.png',
-  '/assets/images/icons/icon-128x128.png',
-  '/assets/images/icons/icon-144x144.png'
+  '/css/style.css',
+  '/js/idb.js', 
+  '/js/index.js', 
+  '/icons/icon-72x72.png', 
+  '/icons/icon-96x96.png', 
+  '/icons/icon-128x128.png', 
+  '/icons/icon-144x144.png', 
+  '/icons/icon-152x152.png', 
+  '/icons/icon-192x192.png', 
+  '/icons/icon-384x384.png', 
+  '/icons/icon-512x512.png' 
 ];
 // Install the service worker
 self.addEventListener('install', function(evt) {
