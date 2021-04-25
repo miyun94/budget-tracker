479bb2d19c38f734fdbc311007b9bc83389fd9b9
echo <miyunniecho@gmail.com>
2021-04-24 21:27:27 -0700

    fix service-worker.js

--- Develop/public/js/idb.js
+++ Develop/public/js/idb.js
@@ -1,4 +1,5 @@
 // create variable to hold db connection
+const indexedDB = window.indexedDB; 
 let db;
 
 // establish a connection to IndexedDB database
@@ -7,7 +8,7 @@ const request = indexedDB.open('budget_tracker', 1);
 // this event will emit if the database version changes
 request.onupgradeneeded = function(event) {
     // save a reference to the database 
-    const db = event.target.result;
+    let db = event.target.result;
     db.createObjectStore('new_budget', { autoIncrement: true });
   };
 
@@ -16,13 +17,11 @@ request.onsuccess = function(event) {
     db = event.target.result;
     // check if app is online
     if (navigator.onLine) {
-      // we haven't created this yet, but we will soon, so let's comment it out for now
-      // uploadPizza();
+      uploadBudget(); 
     }
   };
   
   request.onerror = function(event) {
-    // log error here
     console.log(event.target.errorCode);
   };
 
@@ -30,28 +29,20 @@ request.onsuccess = function(event) {
 function saveRecord(record) {
     // open a new transaction with the database with read and write permissions 
     const transaction = db.transaction(['new_budget'], 'readwrite');
-  
-    // access the object store 
     const budgetObjectStore = transaction.objectStore('new_budget');
-  
-    // add record to your store with add method
     budgetObjectStore.add(record);
   }
 
   function uploadBudget() {
     // open a transaction on your db
     const transaction = db.transaction(['new_budget'], 'readwrite');
-  
-    // access your object store
     const budgetObjectStore = transaction.objectStore('new_budget');
-  
-    // get all records from store and set to a variable
     const getAll = budgetObjectStore.getAll();
   // upon a successful .getAll() execution, run this function
     getAll.onsuccess = function() {
     // if there was data in indexedDb's store, let's send it to the api server
     if (getAll.result.length > 0) {
-      fetch('/api/', {
+      fetch('/api/transaction/bulk', {
         method: 'POST',
         body: JSON.stringify(getAll.result),
         headers: {
@@ -66,9 +57,7 @@ function saveRecord(record) {
           }
           // open one more transaction
           const transaction = db.transaction(['new_budget'], 'readwrite');
-          // access the new_pizza object store
           const budgetObjectStore = transaction.objectStore('new_budget');
-          // clear all items in your store
           budgetObjectStore.clear();
 
           alert('All saved budget has been updated');
